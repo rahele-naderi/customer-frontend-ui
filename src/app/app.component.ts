cabf22451a075123a9188beb8907869713351754
Rahele Naderi <naderi.rahele2017@gmail.com>
2022-11-28 00:30:27 +0330

    add sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { MenuItem } from './models/menu-item.model';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +12,21 @@ import { TranslateService } from '@ngx-translate/core';
 export class AppComponent {
   title = 'customer-frontend-ui';
 
-  constructor(private readonly translateService: TranslateService) {
+  menuItems: MenuItem[];
+
+  constructor(private readonly translateService: TranslateService,
+    private readonly router: Router) {
+    this.menuItems = [];
+    this.menuItems.push({ title: "APP.SIDEBAR.HOME", url: "", icon: "" });
+    this.menuItems.push({ title: "APP.SIDEBAR.ABOUT", url: "", icon: "" });
   }
 
   setLang(lang: string) {
     this.translateService.use(lang);
+  }
+
+  itemClick(url: string, drawer: MatSidenav) {
+    this.router.navigateByUrl(url);
+    drawer.close();
   }
 }
